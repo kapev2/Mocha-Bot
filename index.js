19dac52a50352288c51d3bbca361d32ca6e556bb
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-25 06:38:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://de01-4.uniplex.xyz:6210' });
+  proxy.web(req, res, { target: 'http://de3.bot-hosting.net:21423' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');
