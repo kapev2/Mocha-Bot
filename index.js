6eb6d6cfe0855a3f5e152349cb27e83d9ffa055e
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-26 23:53:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
   proxy.web(req, res, { target: 'http://de01-4.uniplex.xyz:6210' });
 });
-server.listen(6210, () => {
-  console.log('Proxy server is running on http://localhost:6210');
+server.listen(3000, () => {
+  console.log('Proxy server is running on http://localhost:3000');
 });
