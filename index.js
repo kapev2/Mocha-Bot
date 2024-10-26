04a22273357779c91f8eaaa606022fb142237902
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-26 23:59:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+
+// Create a proxy server instance
 const proxy = httpProxy.createProxyServer({});
-const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://de01-4.uniplex.xyz:6210' });
+
+// Handle connection errors
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
+  res.writeHead(502, { 'Content-Type': 'text/plain' });
+  res.end('Bad Gateway');
 });
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  // Forward the request to the target server
+  proxy.web(req, res, { target: 'http://de01-4.uniplex.xyz:6210' }, (error) => {
+    // Handle errors that occur during the proxying process
+    console.error('Error while proxying request:', error);
+    res.writeHead(502, { 'Content-Type': 'text/plain' });
+    res.end('Bad Gateway');
+  });
+});
+
+// Start the server
 server.listen(3000, () => {
   console.log('Proxy server is running on http://localhost:3000');
 });
