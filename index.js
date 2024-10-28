a48a1cd8b63ea3eb82b62b9891ee25f1351d9bf6
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-28 18:55:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25687' });
+  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25687/' });
 });
 server.listen(25687, () => {
   console.log('Proxy server is running on http://localhost:8080');
