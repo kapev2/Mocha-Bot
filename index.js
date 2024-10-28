56f5b116feb86f6c5758f9f607de5c8cb89a1de0
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-28 18:49:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
-
 const proxy = httpProxy.createProxyServer({});
-
-// Error handling for proxy
-proxy.on('error', (err, req, res) => {
-  console.error('Proxy error:', err);
-  res.writeHead(502, { 'Content-Type': 'text/plain' });
-  res.end('Bad Gateway');
-});
-
-// Create the HTTP server
-const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://152.42.220.111:25568' }, (error) => {
-    console.error('Error while proxying request:', error);
-    res.writeHead(502, { 'Content-Type': 'text/plain' });
-    res.end('Bad Gateway');
-  });
+  proxy.web(req, res, { target: 'http://jonellpogi.serv00.net' });
 });
-
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Proxy server is running on http://localhost:${PORT}`);
+server.listen(8080, () => {
+  console.log('Proxy server is running on http://localhost:8080');
 });
