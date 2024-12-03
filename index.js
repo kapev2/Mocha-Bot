1f610250cc999bb80264cd6892a141b2237697e8
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-12-03 18:16:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const httpProxy = require('http-proxy');
 const port = process.env.PORT || 5000;
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25687/' });
+  proxy.web(req, res, { target: 'http://de01.uniplex.xyz:5109/' });
 });
 server.listen(port, () => {
-  console.log('Proxy server is running on http://localhost: 5000');
+  console.log('Proxy server is running on http://localhost: 5109');
 });
